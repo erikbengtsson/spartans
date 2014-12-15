b5e1d0e09eca3fec7f89f9d4dafa6f84c6577015
erikbengtsson <minsk@Minsks-MacBook-Pro.local>
2014-12-15 17:00:28 +0100

    Styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$(function(){
+	// var boxes = $('.scroll'),
+	// $window = $(window);
+	// $window.scroll(function(){
+	// 	var scrollTop = $window.scrollTop();
+	// 	boxes.each(function(){
+	// 		var $this = $(this),
+	// 			scrollspeed = parseInt($this.data('scroll-speed')),
+	// 			val = - scrollTop / scrollspeed;
+	// 		$this.css('transform', 'translateY(' + val + 'px)');
+	// 	});
+	// });
+
+	$('.menu-button').on('click', function(){
+		$('.main-menu').toggleClass('active');
+	});
+});
